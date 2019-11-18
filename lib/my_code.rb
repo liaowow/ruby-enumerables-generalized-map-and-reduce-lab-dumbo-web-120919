e9d189b7a7e6c0690ef18bc479cd16863c7d7880
Annie Liao <liaowow@yahoo.com>
2019-11-18 20:53:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,13 @@ def reduce(arr, num = 0)
   val = num
   i = 0
   while i < arr.length do
-    val += yield(arr[i], num)
+    if yield(arr[i], num)
+      return true
+      elsif !yield(arr[i], num)
+      return false
+    else
+      val += yield(arr[i], num)
+    end
     i += 1
   end
   val
