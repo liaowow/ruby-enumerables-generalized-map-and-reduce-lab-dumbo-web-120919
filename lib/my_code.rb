5536d66b2d27a23bea3b527e563648807cdd6255
Annie Liao <liaowow@yahoo.com>
2019-11-18 21:36:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,13 +9,14 @@ def map(arr)
   new_arr
 end
 
-def reduce(arr, val = true)
+def reduce(arr, num = 0)
+  val = num
   i = 0
   while i < arr.length do
-    if !yield(arr[i], val)
+    if !yield(arr[i], num)
       return false
     else
-    val += yield(arr[i], val = 0)
+    val += yield(arr[i], num = 0)
     end
     i += 1
   end
